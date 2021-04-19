e6f342d531d8a57a7675cbcf0e47b0f6927e12ae
George <georgios.nicola@hotmail.com>
2021-04-19 10:30:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TodoList from './components/TodoList';
 import NavBar from './NavBar';
 
+//Imported NavBar toggle and TodoList via JSX Elements
 function App() {
   return (
     <div className="navBarSection">
